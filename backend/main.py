af518fe54600c944573caff8c2e134f7157c74f2
Sophia Chang <schang7jcu@gmail.com>
2026-05-02 19:48:25 -0700

    fix: add http:// header

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "kirohackathon-kzbn.vercel.app",
+        "http://kirohackathon-kzbn.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
